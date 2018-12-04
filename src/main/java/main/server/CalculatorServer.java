a0deebd2bb667b3f851c2498349bf1e92fe7bbfd
Bartosz Osowski <bosowski04@gmail.com>
2018-12-04 03:37:03 +0000

    Initial commit

---
+++ src/main/java/main/server/CalculatorServer.java
@@ -0,0 +1,45 @@
+package main.server;
+
+import main.Calculator;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class CalculatorServer extends UnicastRemoteObject implements Calculator {
+
+  CalculatorServer() throws RemoteException{
+    super();
+    LocateRegistry.createRegistry(1099).rebind("Calculator", this);
+    System.out.println("Successfully bound calculator to registry.");
+  }
+
+ public static void main(String args[]){
+   try {
+     new CalculatorServer();
+   } catch (RemoteException e) {
+     System.out.println("Error while trying to instantiate " + CalculatorServer.class.getName() + ": " + e.getMessage());
+     e.printStackTrace();
+   }
+ }
+
+  @Override
+  public double add(double a, double b) {
+    return a + b;
+  }
+
+  @Override
+  public double subtract(double a, double b) {
+    return a - b;
+  }
+
+  @Override
+  public double multiply(double a, double b) {
+    return a * b;
+  }
+
+  @Override
+  public double divide(double a, double b) {
+    return a / b;
+  }
+}
---
+++ src/test/java/main/server/CalculatorServerTest.java
@@ -0,0 +1,48 @@
+package main.server;
+
+
+import main.Calculator;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculatorServerTest {
+
+  private Calculator calculator;
+
+  @BeforeAll
+  static void launchServer() throws Exception {
+    new CalculatorServer();
+  }
+
+  @BeforeEach
+  void setUp() throws RemoteException, NotBoundException, MalformedURLException {
+    calculator = (Calculator) Naming.lookup("Calculator");
+  }
+
+  @Test
+  void testAdd() throws RemoteException {
+    assertEquals(1, calculator.add(0.5, 0.5), 0.1);
+  }
+
+  @Test
+  void testSubtract() throws RemoteException {
+    assertEquals(1, calculator.subtract(2,1), 0.1);
+  }
+
+  @Test
+  void testMultiply() throws RemoteException {
+    assertEquals(4, calculator.multiply(2,2), 0.1);
+  }
+
+  @Test
+  void testDivide() throws RemoteException {
+    assertEquals(1, calculator.divide(2,2), 0.1);
+  }
+}
